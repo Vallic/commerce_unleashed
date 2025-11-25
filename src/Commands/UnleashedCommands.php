6cc1c73d123c0e07469624dfb1e73fce04ccebf5
Valentino Međimorec <valentino@medimorec.com>
2025-11-25 21:48:25 +0100

    Expand drush command for syncing products

--- src/Commands/UnleashedCommands.php
+++ src/Commands/UnleashedCommands.php
@@ -4,6 +4,7 @@ namespace Drupal\commerce_unleashed\Commands;
 
 use Drupal\commerce_unleashed\UnleashedManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\KeyValueStore\KeyValueFactory;
 use Drush\Commands\DrushCommands;
 
 class UnleashedCommands extends DrushCommands {
@@ -18,14 +19,17 @@ class UnleashedCommands extends DrushCommands {
    */
   protected UnleashedManagerInterface $unleashedManager;
 
+  protected KeyValueFactory $keyValueFactory;
+
   /**
    * Constructs a new UnleashedCommands object.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, UnleashedManagerInterface $unleashed_manager) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, UnleashedManagerInterface $unleashed_manager, KeyValueFactory $keyValueFactory) {
     parent::__construct();
 
     $this->entityTypeManager = $entity_type_manager;
     $this->unleashedManager = $unleashed_manager;
+    $this->keyValueFactory = $keyValueFactory;
   }
 
   /**
@@ -38,10 +42,19 @@ class UnleashedCommands extends DrushCommands {
    *
    * @command commerce_unleashed:sync:products
    */
-  public function syncProducts(string $sku = 'all', array $options = ['query' => NULL]): void {
+  public function syncProducts(string $sku = 'all', int $full_sync = 0, array $options = ['query' => NULL]): void {
     if ($sku === 'all') {
-      // Use // to escape the &.
       $query = $options['query'] ? str_replace('//', '&', $options['query']) : 'brief=true';
+
+      if (empty($full_sync)) {
+        $last_product_sync = $this->keyValueFactory->get('commerce_unleashed')->get('products') ?? 0;
+
+        if ($last_product_sync > 0) {
+          $last_modified_date = date('Y-m-d', $last_product_sync);
+          $query .= '&modifiedSince=' . $last_modified_date;
+          $this->keyValueFactory->get('commerce_unleashed')->set('products', time());
+        }
+      }
       $this->unleashedManager->syncProducts($query);
     }
     else {
